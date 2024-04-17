e2bda4a9b9d3b1273a6fe76a1ade57e694c291f9
kirill kurochkin <godoffirekirill@gmail.com>
2024-04-17 18:49:13 +0200

    correct

--- HW02.playground/Contents.swift
+++ HW02.playground/Contents.swift
@@ -11,19 +11,19 @@ print("\(yeadOfBirth)")
 let someNumbers = "34567"
 
 do {
-    if ((Int(someNumbers)! / 3) != 0) {
+    if ((Int(someNumbers)! % 3) == 0) {
         print("someNumbers", "divisible", "3")
     };
-    if ((Int(someNumbers)! / 4) != 0) {
+    if ((Int(someNumbers)! % 4) == 0) {
         print("someNumbers", "divisible", "4")
     };
-    if ((Int(someNumbers)! / 5) != 0) {
+    if ((Int(someNumbers)! % 5) == 0) {
         print("someNumbers", "divisible", "5")
     };
-    if ((Int(someNumbers)! / 6) != 0) {
+    if ((Int(someNumbers)! % 6) == 0) {
         print("someNumbers", "divisible", "6")
     };
-    if ((Int(someNumbers)! / 7) != 0) {
+    if ((Int(someNumbers)! % 7) == 0) {
         print("someNumbers", "divisible", "7")
     };
 }
@@ -42,15 +42,15 @@ func performDivision(divided: Divided, someNumbers: String) -> Int? {
 
     switch divided {
     case .three:
-        return unwrappedNumber / 3
+        return unwrappedNumber % 3
     case .four:
-        return unwrappedNumber / 4
+        return unwrappedNumber % 4
     case .five:
-        return unwrappedNumber / 5
+        return unwrappedNumber % 5
     case .six:
-        return unwrappedNumber / 6
+        return unwrappedNumber % 6
     case .seven:
-        return unwrappedNumber / 7
+        return unwrappedNumber % 7
     }
 }
 print("\(String(describing: performDivision(divided: .three, someNumbers: someNumbers))))")
@@ -63,9 +63,9 @@ print("\(String(describing: performDivision(divided: .seven, someNumbers: someNu
 
 
 for divider in 3 ... 7 {
-    var result = (Int(someNumbers)! / divider)
-    if result != 0 {
-        print("the \(someNumbers)", "divisible", "by \(divider)")
+    var result = (Int(someNumbers)! % divider)
+    if result == 0 {
+        print("the \(result)", "divisible", "by \(divider)")
     }
 }
 
